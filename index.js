ab8d6297b3cf20568908831e97f2f6a483873e7b
Matthew <158129347+Montoose@users.noreply.github.com>
2024-08-06 01:06:15 -0500

    Create function to calculate round winner
    This function compares the value of the player's
    choice with the value of the computer's choice.
    Also creates variables that will hold the score
    for the computer and for the player. Adds an
    console printout of the computer's choice.

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
 function getComputerChoice() {
     let number = Math.round(Math.random() * 100);
     if (number <= 33) {
-        return "Scissors";
+        console.log("Computer chose Scissors!")
+        return "scissors";
     }
     else if (number > 33 && number <= 66) {
-        return "Rock";
+        console.log("Computer chose Rock!")
+        return "rock";
     }
     else {
-        return "Paper";
+        console.log("Computer chose Paper!")
+        return "paper";
     }
 }
 
 function getHumanChoice() {
     let choice = prompt("Make your choice! ");
-    return choice
+    return choice.toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound (computer, human) {
+    if (human == computer) {
+        console.log("We have a tie!");
+    }
+    else if (human == "rock" && computer == "paper") {
+        console.log("Computer Wins!");
+        computerScore++;
+    }
+    else if (human == "rock" && computer == "scissors") {
+        console.log("Human Wins!");
+        humanScore++;
+    }
+    else if (human == "paper" && computer == "rock") {
+        console.log("Human Wins!");
+        humanScore++;
+    }
+    else if (human == "paper" && computer == "scissors") {
+        console.log("Computer Wins!");
+        computerScore++;
+    }
+    else if (human == "scissors" && computer == "rock") {
+        console.log("Computer Wins!");
+        computerScore++;
+    }
+    else if (human == "scissors" && computer == "paper") {
+        console.log("Human Wins!");
+        humanScore++;
+    }
+}
+
+playRound(humanChoice, computerChoice);
